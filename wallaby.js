8b72c310165dbf830ce1d5393fb9669d1be3f37f
Einar Ingebrigtsen <einar@dolittle.com>
2019-03-09 20:07:37 +0100

    Making it compatible with new Build/Wallaby

--- wallaby.js
+++ wallaby.js
@@ -1,11 +1,10 @@
 const wallaby = require('@dolittle/build/dist/wallaby/node')
-module.exports = wallaby('.',
-    (config) => {
-
-    },
-    () => {
-        require('aurelia-polyfills');
-        const { globalize } = require('aurelia-pal-nodejs')
-        globalize();
-    }
-);
+module.exports = function (settingsCallback) {
+    return wallaby(settingsCallback,
+        () => {
+            require('aurelia-polyfills');
+            const { globalize } = require('aurelia-pal-nodejs')
+            globalize();
+        }
+    );
+}
