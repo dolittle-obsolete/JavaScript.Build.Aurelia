70a1ff6d843228dbbd4bca6edc24bb55991bdd55
Einar Ingebrigtsen <einar@dolittle.com>
2019-03-09 19:03:59 +0100

    Making use of the new setup method
    #18

--- wallaby.js
+++ wallaby.js
@@ -1,31 +1,11 @@
-const AureliaPlugin = require('aurelia-webpack-plugin').AureliaPlugin;
-const DefinePlugin = require('webpack').DefinePlugin;
+const wallaby = require('@dolittle/build/dist/wallaby/node')
+module.exports = wallaby('.',
+    (config) => {
 
-const wallaby = require('@dolittle/build/wallaby')
-const setupFile = 'node_modules/@dolittle/build.aurelia/aureliaSetup.js';
-
-module.exports = (baseFolder, webpackPostprocessorCallback, wallabySetingsCallback) => {
-    if( !baseFolder ) baseFolder = 'features';
-    return wallaby(baseFolder, webpack => {
-
-        webpack.entryPatterns.unshift(setupFile);
-        webpack.module.rules = webpack.module.rules.concat([
-            { test: /\.html$/i, loader: 'html-loader' },
-            { test: /\.css$/i, issuer: [{ not: [{ test: /\.html$/i }] }], use: ['style-loader', 'css-loader'] },
-            { test: /\.css$/i, issuer: [{ test: /\.html$/i }], use: 'css-loader' },   
-        ]);
-    
-        webpack.plugins = webpack.plugins.concat([
-            new DefinePlugin({ AURELIA_WEBPACK_2_0: undefined }),
-            new AureliaPlugin()
-        ]);
-
-        if( typeof webpackPostprocessorCallback == 'function' ) webpackPostprocessorCallback(webpack);
-
-    }, wallaby => {
-        wallaby.files.push({ pattern: setupFile, load: false });
-
-        if( typeof wallabySetingsCallback == 'function' ) wallabySetingsCallback(wallaby);
-    });
-};
-
+    },
+    () => {
+        require('aurelia-polyfills');
+        const { globalize } = require('aurelia-pal-nodejs')
+        globalize();
+    }
+);
